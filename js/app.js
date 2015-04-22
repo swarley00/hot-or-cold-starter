a92f82e3636b2321c07d310e3d9a8a2abbbef68c
swarley00 <jason.caryl@gmail.com>
2015-04-21 22:47:40 -0400

    Change button from submit to reset, tried to figure out the range logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 	
+	var newBtn = $(".new");
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,7 +14,8 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-	var newBtn = $(".new");
+  	newGame();
+  	
 	newBtn.click(function () {
 		newGame();
 	}); 
@@ -21,22 +24,30 @@ $(document).ready(function(){
 
 function newGame () {
 	generateNumber();
+	feedback();
 }
 
 function generateNumber () {
 	var randomNumber = Math.round((Math.random()*100));
+	console.log(randomNumber);
 	return randomNumber;
 }
 
 function feedback () {
 	var userGuess = $("#userGuess").val();
-	if () {
-
+	if (userGuess <= randomNumber - 50) {
+		console.log("Ice Cold");
 	}
-	else if () {
-
+	else if (userGuess > randomNumber - 30 && userGuess < randomNumber - 50) {
+		console.log("Cold");
 	}
-	else {
-
+	else if (userGuess > randomNumber - 20 && userGuess < randomNumber - 30) {
+		console.log("Warm");
+	}
+	else if (userGuess > randomNumber - 10 && userGuess < randomNumber - 20) {
+		console.log("Hot");
+	}
+	else if (userGuess > randomNumber - 1 && userGuess < randomNumber - 10) {
+		console.log("Very Hot");
 	}
 }

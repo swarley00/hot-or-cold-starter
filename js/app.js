e3fd41c3d0e6da3eac51b3a3c3efaf9f9398e417
Jason Caryl <jason.caryl@gmail.com>
2015-04-30 21:57:20 -0400

    Replace text in feedback div

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var newBtn = $(".new");
 	var guessBtn = $("#guessButton");
 	var randomNumber = generateNumber();
+	var feedBack = $("#feedback");
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -37,20 +38,30 @@ $(document).ready(function(){
 	function feedback () {
 		var userGuess = $("#userGuess").val();
 		var difference = Math.abs(userGuess - randomNumber);
+		var iceCold = "<h2 id='feedback'>Ice Cold</h2>";
+		var cold = "<h2 id='feedback'>Cold</h2>";
+		var warm = "<h2 id='feedback'>Warm</h2>";
+		var hot = "<h2 id='feedback'>Hot</h2>";
+		var veryHot = "<h2 id='feedback'>Very Hot</h2>";
 
 		if (difference >= 50) {
+			feedBack.replaceWith(iceCold);
 			console.log("Ice Cold");
 		}
 		else if (difference >= 30 && difference <= 50) {
+			feedBack.replaceWith(cold);
 			console.log("Cold");
 		}
 		else if (difference >= 20 && difference <= 30) {
+			feedBack.replaceWith(warm);
 			console.log("Warm");
 		}
 		else if (difference >= 10 && difference <= 20) {
+			feedBack.replaceWith(hot);
 			console.log("Hot");
 		}
 		else if (difference >= 1 && difference <= 10) {
+			feedBack.replaceWith(veryHot);
 			console.log("Very Hot");
 		}
 		else {
